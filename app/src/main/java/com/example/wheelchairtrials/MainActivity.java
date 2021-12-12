14a9dd3c87cace36b5e6eca7bd5c1bf5f3ff9c0e
Pradyumna <reddy.pradyu@gmail.com>
2021-12-12 02:40:27 -0500

    Added the graph

--- app/src/main/java/com/example/wheelchairtrials/MainActivity.java
+++ app/src/main/java/com/example/wheelchairtrials/MainActivity.java
@@ -32,8 +32,7 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.toString();
     private boolean startRecording = false;
-    private static final String COMMA_DELIMITER = ",";
-
+    private boolean toggle = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,10 +43,12 @@ public class MainActivity extends AppCompatActivity {
             startRecording = on;
         });
         final Button resultButton = (Button) findViewById(R.id.button2);
-        resultButton.setClickable(false);
+        resultButton.setEnabled(toggle);
         button.setOnCheckedChangeListener(((compoundButton, off) -> {
-            resultButton.setClickable(true);
+            toggle = !toggle;
+            resultButton.setEnabled(toggle);
         }));
+
 
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
@@ -70,62 +71,8 @@ public class MainActivity extends AppCompatActivity {
         }, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    // http://phrogz.net/js/framerate-independent-low-pass-filter.html
-    public static void smoothArray(Double[] values, int smoothing) {
-        Double value = values[0]; // start with the first input
-        for (int i=1; i < values.length; i++){
-            Double currentValue = values[i];
-            value += (currentValue - value) / smoothing;
-            values[i] = value;
-        }
-    }
-
     public void showResults(View view) throws FileNotFoundException, ParseException {
         Intent intent = new Intent(this, Result.class);
-        List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(getFilesDir().getAbsolutePath() + "/data.csv"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(COMMA_DELIMITER);
-                records.add(Arrays.asList(values));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        SimpleDateFormat[] timeStamps = new SimpleDateFormat[records.size()];
-        Double[] xVal = new Double[records.size()];
-        Double[] yVal = new Double[records.size()];
-        Double[] zVal = new Double[records.size()];
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy h:mm:ss", Locale.ENGLISH);
-        Date[] dateStamp = new Date[records.size()];
-        for (int i = 0; i < records.size(); i++) {
-            dateStamp[i] = sdf.parse(records.get(i).get(0));
-            xVal[i] = Double.parseDouble(records.get(i).get(1));
-            yVal[i] = Double.parseDouble(records.get(i).get(2));
-            zVal[i] = Double.parseDouble(records.get(i).get(3));
-        }
-//        System.out.println(Arrays.toString(xVal));
-        // get these from user input
-        double wheelDiam = 0.1412;
-        String wheel_side = "left";
-
-        double weightLoad = 100;
-        double sampFreq = 51.2;
-
-
-        // filter the relevant accelerations
-        smoothArray(xVal, 2);
-        smoothArray(yVal, 2);
-        smoothArray(zVal, 2);
-
-        //for finding magnitude
-        Double[] magnitude = new Double[xVal.length];
-        for (int i = 0; i < xVal.length; i++) {
-            Double mag = (xVal[i] * xVal[i]) + (yVal[i] * yVal[i]) + (zVal[i] * zVal[i]);
-            magnitude[i] = Math.sqrt(mag);
-        }
-
-        smoothArray(magnitude, 2);
         startActivity(intent);
     }
     private void saveData(SensorEvent event) {
